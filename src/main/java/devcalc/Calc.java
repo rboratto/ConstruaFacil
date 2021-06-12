a0e36347f961431c47f65111ab463bddd2bbd1fd
rboratto <rboratto71@gmail.com>
2021-06-11 21:43:19 -0300

    construa facil inicio

---
+++ src/main/java/devcalc/Calc.java
@@ -0,0 +1,18 @@
+//1 - pacote
+
+package devcalc;
+//2 - bibliotecas
+
+//3- classe
+public class Calc {
+
+    //3.1 - atributos
+
+    //3.2 - métodos e funções
+    public static void main(String[] args){
+        System.out.println("Passou pelo método main");
+    }
+    public static void somarDoisNumeros(){
+        System.out.println("Passou pelo método somarDoisNumeros");
+    }
+}
